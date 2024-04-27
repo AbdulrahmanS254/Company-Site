06f8d4fcf6229f5129b03a9b16c4e9f8d98da991
Abdulrahman Shawky <hawk18922@gmail.com>
2024-04-27 14:14:59 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+window.bootstrap = require("bootstrap/dist/js/bootstrap.bundle.js");
+import "../sass/style.scss";
+import "@fortawesome/fontawesome-free/js/all.js"
+
+$(function() {
+    $('.thumbnail-2').hover(function() {
+        $(this).find('.project-category').hide();
+        $(this).find('.caption-2').slideDown(300);
+    },
+    function(){
+        $(this).find('.caption-2').slideUp(300);
+        $(this).find('.project-category').show();
+    })
+})
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  stats: "errors only",
+
+  entry: "./src/js/index.js",
+
+  output: {
+    publicPath: "/",
+    filename: "js/bundle.js",
+    path: __dirname + "/build",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false,
+            },
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "./images/[name][ext]",
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "./fonts/[name][ext]",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/style.css",
+    }),
+  ],
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "build"),
+    },
+    compress: true,
+    port: 9000,
+    devMiddleware: {
+      writeToDisk: true,
+    },
+    open: true,
+    hot: false,
+  },
+};

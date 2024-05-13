a3dfd17bafc7ce9bded2dc70d239bf8de327a220
Abdulrahman Shawky <hawk18922@gmail.com>
2024-05-13 18:36:42 +0300

    blog-details page and add-blog page done

--- webpack.config.js
+++ webpack.config.js
@@ -66,6 +66,14 @@ module.exports = {
       template: "./src/blog.html",
       filename: "blog.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/blog-details.html",
+      filename: "blog-details.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/add-blog.html",
+      filename: "add-blog.html",
+    }),
     new MiniCssExtractPlugin({
       filename: "css/style.css",
     }),

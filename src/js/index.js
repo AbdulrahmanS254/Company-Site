8b0f0196946ad21bf59870c376aedeb13787ac7b
Abdulrahman Shawky <hawk18922@gmail.com>
2024-05-19 21:05:31 +0300

    The website is finished

--- src/js/index.js
+++ src/js/index.js
@@ -13,6 +13,19 @@ $(function() {
     })
 })
 
+// Adding and deleting active class in navbar items
+var pathname = window.location.pathname;
+$('.navbar-nav > li > a[href="'+pathname+'"]').addClass('active');
+console.log(pathname);
+
+if(pathname == '/blog-details.html' || pathname == '/add-blog.html') {
+    $('.navbar-nav > li > a[href="/blog.html"]').addClass('active');
+}
+
+if(pathname == '/project-details.html') {
+    $('.navbar-nav > li > a[href="/projects.html"]').addClass('active');
+}
+
 var date = new Date();
 var year = date.getFullYear();
 document.getElementById("copyright-year").innerHTML = year;
